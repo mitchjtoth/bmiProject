7b8f333c38dcd55707f7e73c70d787e4337d74dd
mitchjtoth <mitchjtoth@gmail.com>
2023-04-15 08:08:45 -0500

    added hosts

--- BMIfinder/settings.py
+++ BMIfinder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vzs4!5gc@fxnsc705pmar_5n#x0**a3dx1azg1%#fc#5%#8!#(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver']
+ALLOWED_HOSTS = ['127.0.0.1:8000','127.0.0.1','testserver']
 
 
 # Application definition
