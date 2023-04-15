17dcd9984ba037db1b786c83f32608df6530331d
mitchjtoth <mitchjtoth@gmail.com>
2023-04-15 15:03:56 -0500

    changed tests.py

--- bmiCalc/tests.py
+++ bmiCalc/tests.py
@@ -10,22 +10,6 @@ def test_one():
 
     assert 1 == 1
 
-
-# def test_calculate_bmi():
-#     # Create a request object with POST data
-#     data = {'weight': '150', 'height_feet': '5', 'height_inches': '6'}
-#     request = RequestFactory().post('/', data=data)
-    
-#     # Call the view function and get the response
-#     response = calculate_bmi(request)
-#     context = response.context
-#     # Check that the response has the expected content
-#     assert response.status_code == 200
-#     assert 'bmi' in context
-#     assert 'bmiResult' in context
-#     assert context['bmi'] == 24.8
-#     assert context['bmiResult'] == 'Your bmi is normal weight'
-
 def test_calculate_bmi_normal():
     client = Client()
     response = client.post('', {'weight': '150', 'height_feet': '5', 'height_inches': '8'})
