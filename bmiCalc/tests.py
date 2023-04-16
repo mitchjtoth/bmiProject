b4a60ea6e61f51fa503d46890cc09d5248f8d068
mitchjtoth <mitchjtoth@gmail.com>
2023-04-16 02:29:45 -0500

    added tests

--- bmiCalc/tests.py
+++ bmiCalc/tests.py
@@ -71,3 +71,69 @@ def test_calculate_bmi_value_error():
     response = client.post(url, {'weight': '1001', 'height_feet': '0', 'height_inches': '0'})
     assert response.status_code == 200
     assert 'Please try again with valid inputs' in response.content.decode('utf-8')
+
+@pytest.fixture
+def client():
+    client = Client()
+    return client
+
+def test_calculate_bmi(client):
+    response = client.post('', {'weight': 150, 'height_feet': 5, 'height_inches': 8})
+    assert response.status_code == 200
+    assert 'bmi' in response.context
+    assert 'bmiResult' in response.context
+    assert response.context['bmi'] == 23.4
+    assert response.context['bmiResult'] == 'Your bmi is normal weight'
+
+
+def test_calculate_bmi_with_missing_fields(client):
+    response = client.post('', {'weight': '', 'height_feet': 5, 'height_inches': 8})
+    assert response.status_code == 200
+    assert 'message' in response.context
+    assert response.context['message'] == "could not convert string to float: ''"
+
+
+def test_calculate_bmi_with_invalid_fields(client):
+    response = client.post('', {'weight': 1500, 'height_feet': -5, 'height_inches': 20})
+    assert response.status_code == 200
+    assert 'message' in response.context
+    assert response.context['message'] == 'Please try again with valid inputs'
+
+
+def test_calculate_bmi_with_high_bmi(client):
+    response = client.post('', {'weight': 1000, 'height_feet': 5, 'height_inches': 8})
+    assert response.status_code == 200
+    assert 'message' in response.context
+    assert response.context['message'] == 'Error: BMI cannot be that high of a number. Please try new values'
+
+#Need to fix integration tests 
+
+# from django.test import LiveServerTestCase
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+# class BMITest(LiveServerTestCase):
+    
+#     def setUp(self):
+#         self.driver = webdriver.Firefox()
+        
+#     def tearDown(self):
+#         self.driver.quit()
+        
+#     def test_calculate_bmi(self):
+#         self.driver.get(self.live_server_url)
+#         self.assertEqual(self.driver.title, "BMI Calculator")
+        
+#         weight_field = self.driver.find_element_by_name('weight')
+#         height_feet_field = self.driver.find_element_by_name('height_feet')
+#         height_inches_field = self.driver.find_element_by_name('height_inches')
+        
+#         weight_field.send_keys('150')
+#         height_feet_field.send_keys('5')
+#         height_inches_field.send_keys('7')
+        
+#         submit_button = self.driver.find_element_by_xpath('//input[@type="submit"]')
+#         submit_button.send_keys(Keys.RETURN)
+        
+#         bmi_element = self.driver.find_element_by_xpath('//p[contains(text(), "Your BMI is ")]')
+#         self.assertIsNotNone(bmi_element)
